e0578bd8cfb673f7830fa94f2293d52d52797272
Khushi Gill <khushi.25b01010092@abes.ac.in>
2026-08-08 21:22:52 +0530

    feat: render garden plant composition

--- app/components/garden/GardenPreview.tsx
+++ app/components/garden/GardenPreview.tsx
@@ -4,39 +4,39 @@ import Plant from "./Plant";
 
 export default function GardenPreview() {
   return (
-    <section className="relative mt-16 w-full max-w-4xl">
-      <div className="relative h-72 overflow-hidden rounded-[2rem] border border-border-soft bg-gradient-to-b from-[#dff0db] via-[#eaf4e7] to-[#d4e7ce] shadow-xl shadow-[#31543b]/10">
+    <section className="w-full">
+      <div className="relative h-[420px] w-full overflow-hidden rounded-[2rem] bg-[#dfead8] shadow-sm ring-1 ring-black/5">
         <GardenSky />
 
         {/* Plants */}
         <div className="absolute inset-x-0 bottom-8 z-10 h-52">
-  {/* Left sprout */}
-  <div className="absolute bottom-0 left-[4%] sm:left-[12%]">
-    <Plant type="sprout" size="sm" />
-  </div>
+          {/* Left sprout */}
+          <div className="absolute bottom-2 left-[10%]">
+            <Plant type="sprout" />
+          </div>
 
-  {/* Left flower */}
-  <div className="absolute bottom-0 left-[20%] sm:left-[28%]">
-    <Plant type="flower" size="md" />
-  </div>
+          {/* Left flower */}
+          <div className="absolute bottom-1 left-[25%]">
+            <Plant type="flower" />
+          </div>
 
-  {/* Main tree */}
-<div className="absolute bottom-0 left-1/2 z-0 -translate-x-1/2">
-    <Plant type="tree" size="lg" />
-  </div>
+          {/* Main tree */}
+          <div className="absolute bottom-0 left-1/2 -translate-x-1/2">
+            <Plant type="tree" />
+          </div>
 
-  {/* Right flower */}
-  <div className="absolute bottom-0 right-[20%] sm:right-[28%]">
-    <Plant type="flower" size="md" />
-  </div>
+          {/* Right flower */}
+          <div className="absolute bottom-1 right-[25%]">
+            <Plant type="flower" />
+          </div>
 
-  {/* Right sprout */}
-  <div className="absolute bottom-0 right-[4%] sm:right-[12%]">
-    <Plant type="sprout" size="sm" />
-  </div>
-</div>
+          {/* Right sprout */}
+          <div className="absolute bottom-2 right-[10%]">
+            <Plant type="sprout" />
+          </div>
 
-        <GardenGround />
+          <GardenGround />
+        </div>
       </div>
 
       <p className="mt-5 text-center text-sm text-text-muted">
